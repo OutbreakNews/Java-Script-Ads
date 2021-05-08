9615ad225dd82a67868be3cb76c1e908da5eb1c8
OutbreakNews <83887724+OutbreakNews@users.noreply.github.com>
2021-05-09 05:24:26 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+ atOptions = {
+  'key' : '8159c769fa7e6ff1c207c414b03aecef',
+  'format' : 'iframe',
+  'height' : 250,
+  'width' : 300,
+  'params' : {}
+ };
+ document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://steadydonut.com/8159c769fa7e6ff1c207c414b03aecef/invoke.js"></scr' + 'ipt>');
+</script>
